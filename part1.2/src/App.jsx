25adc761f00e20d84a8c2ed171a54860d6e41df2
Michael Nunes Jacobs <shigerun@web.de>
2024-01-03 18:11:29 +0100

    Part a of Exercise 1

---
+++ part1.2/src/App.jsx
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return(<>
+  <h1>{props.course}</h1>
+  </>
+  )
+}
+const Part = (props) => {
+  return(
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <>
+      <Part part={props.part[0]} exercise={props.exercise[0]}/>
+      <Part part={props.part[1]} exercise={props.exercise[1]}/>
+      <Part part={props.part[2]} exercise={props.exercise[2]}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <>
+    <p>Number of exercises {props.exercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={[part1,part2,part3]} exercise={[exercises1,exercises2,exercises3]}/>
+      <Total exercises={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
