cf0a22d024ca2574d618b023b56607797d756314
Michael Nunes Jacobs <shigerun@web.de>
2024-05-19 19:50:21 +0200

    solved 2.16 phonebook step 11

--- part2 thephonebook/src/App.jsx
+++ part2 thephonebook/src/App.jsx
@@ -3,26 +3,29 @@ import CheckForDuplicate from "./components/CheckForDuplicate";
 import Phonebook from "./components/Phonebook";
 import Search from "./components/Search";
 import Input from "./components/Input";
+import Notification from "./components/Notification";
 import phonebookService from "./services/phonebook";
+
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState(null);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [query, setQuery] = useState([]);
+  const [infoMsg, setInfomsg] = useState(null);
   useEffect(() => {
     phonebookService
       .getAll()
       .then((initialPhonebook) => setPersons(initialPhonebook))
       .catch((e) => console.error("intialGet crashed", e));
   }, [persons]);
-  const handleDelete = (props) =>{
+  const handleDelete = (props) => {
     window.confirm("do you want to delete this entry?") &&
-    phonebookService
-      .removeEntry(props)
-      .catch((e) =>
-        console.error("the phonebook entry is already deleted ", e)
-      );
-  }
+      phonebookService
+        .removeEntry(props)
+        .catch((e) =>
+          console.error("the phonebook entry is already deleted ", e)
+        );
+  };
   const handleForm = (e) => {
     e.preventDefault();
     const newPerson = {
@@ -35,6 +38,10 @@ const App = () => {
           .create(newPerson)
           .then((resp) => setPersons(persons.concat(resp.data)))
           .catch((e) => console.error("addition of a new entry crashed", e));
+        setInfomsg(`Added '${newPerson.name}' to the phonebook`);
+        setTimeout(() => {
+          setInfomsg(null);
+        }, 5000);
         setNewName("");
         setNewNumber("");
         break;
@@ -81,6 +88,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={infoMsg} />
       <div>
         <Input
           text={"filter book: "}
@@ -110,7 +118,10 @@ const App = () => {
       <div>
         <h2>Numbers</h2>
         <ul>
-          <Phonebook onClick={handleDelete} entries={query.length <= 0 ? persons : query} />
+          <Phonebook
+            onClick={handleDelete}
+            entries={query.length <= 0 ? persons : query}
+          />
         </ul>
       </div>
     </div>
