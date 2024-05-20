fee2c1a300255b3f3748fecbd9bafa825e82e4c9
Michael Nunes Jacobs <shigerun@web.de>
2024-05-20 17:27:08 +0200

    set up for hosting on render

--- part3/webserver/index.js
+++ part3/webserver/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -69,7 +71,7 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
