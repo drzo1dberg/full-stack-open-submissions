22d25eefe1b2bccc3bfe53dd8cf239a93d36b3be
Michael Nunes Jacobs <shigerun@web.de>
2024-05-21 13:22:54 +0200

    set up for hosting on render pt 3

--- part3/phonebook backend (3.1 - 3.8.)/index.js
+++ part3/phonebook backend (3.1 - 3.8.)/index.js
@@ -101,7 +101,7 @@ app.post("/api/persons", (req, resp) => {
   phonebook = phonebook.concat(phonebookEntry);
   resp.json(phonebook);
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
