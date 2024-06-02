e7025849ca978e167880499c1b65a478d2efca8c
Michael Nunes Jacobs <shigerun@web.de>
2024-06-02 22:40:51 +0200

    uibuild

--- part3/notes app/webserver/index.js
+++ part3/notes app/webserver/index.js
@@ -3,9 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const Note = require("./models/note");
-app.use(express.json());
-app.use(cors());
-
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -13,26 +10,44 @@ const requestLogger = (request, response, next) => {
   console.log("---");
   next();
 };
+app.use(express.static("dist"));
+app.use(express.json());
 app.use(requestLogger);
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+app.use(cors());
 app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
 });
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -52,9 +67,18 @@ app.post("/api/notes", (request, response) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
-
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+// handler of requests with result to errors
+app.use(errorHandler);
